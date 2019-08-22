cd20ffc0cc9fc62553ebba3fcb220e04ac14ef99
Nina <nina.manalo19@gmail.com>
2019-08-22 10:43:31 +0400

    Updates

---
+++ precache-manifest.2c11b73ccdd56fb58d75dcb5a23417d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e83e6fad727b3a7d6b89c4be3f30412",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "576d72ae65545924d753",
+    "url": "/home/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "c2dbb26f779d8a7d84ba",
+    "url": "/home/static/css/main.991b61b3.chunk.css"
+  },
+  {
+    "revision": "576d72ae65545924d753",
+    "url": "/home/static/js/2.e35d6412.chunk.js"
+  },
+  {
+    "revision": "c2dbb26f779d8a7d84ba",
+    "url": "/home/static/js/main.7b4b0631.chunk.js"
+  },
+  {
+    "revision": "7e5a868ddb76c328a2b9",
+    "url": "/home/static/js/runtime~main.4e093761.js"
+  },
+  {
+    "revision": "28752a375ee349f3fc6889ad915f7469",
+    "url": "/home/static/media/home-top.28752a37.jpg"
+  }
+]);
